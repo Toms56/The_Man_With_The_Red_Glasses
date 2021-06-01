bde51abeeceec02b0e3f249093845246f9b59754
Tanneau <tanneauthomas56@gmail.com>
2021-06-01 14:05:44 +0200

    reglage Lights

---
+++ The_Man_Of_The_Red_Glasses_Project/Assets/Scripts/EmissiveMaterial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissiveMaterial : MonoBehaviour
+{
+    private Color color;
+    private float f = 0.4f;
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        GetComponent<Renderer>().material.SetColor("_EmissionColor",color*f);
+        rend.material.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        f = getIntensity();
+        Color color = Color.red * f;
+        rend.material.SetColor("_EmissionColor", color);
+    }*/
+}
